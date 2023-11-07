052697e3eab70cdd51c79fa93b6b01076380f10d
Hugo Pernet <hugopernet@ssw.fr>
2023-11-07 14:52:46 +0100

    added a second copy button to get the details in French

--- src/content-script.js
+++ src/content-script.js
@@ -1,3 +1,8 @@
+const COUNTRY_CODES={
+    AU:"au",
+    FR:"fr"
+};
+
 async function main() {
     const ratesCSV = chrome.runtime.getURL("src/assets/rates.csv");
     let csvData;
@@ -29,30 +34,46 @@ async function main() {
 
     //TOOD: remove timeout and find a way to wait for whole page to load
     setTimeout(() => {
-        const iconImg = document.createElement("img");
-        iconImg.src = chrome.runtime.getURL("src/assets/icons/au_copy.svg");
-        iconImg.classList.add("copy-img");
+        const copyButtonsContainer = document.createElement('div');
+        copyButtonsContainer.classList.add("copy-buttons-container");
 
-        const copyButton = document.createElement("button");
-        copyButton.setAttribute('title','Copy details');
-        copyButton.className = "copy-button";
-        copyButton.appendChild(iconImg);
+        const copyButtonAu = document.createElement("button");
+        setCopyButton(copyButtonAu,COUNTRY_CODES.AU);
+        const copyButtonFr = document.createElement("button");
+        setCopyButton(copyButtonFr,COUNTRY_CODES.FR);
+
+        copyButtonsContainer.appendChild(copyButtonAu);
+        copyButtonsContainer.appendChild(copyButtonFr);
 
         const profileImage = document.querySelector("img.profile-image");
         const profileImageDiv = profileImage.parentNode;
-        profileImageDiv.appendChild(copyButton);
+        profileImageDiv.appendChild(copyButtonsContainer);
 
-        copyButton.addEventListener("click", async function () {
-            //copying to clipboard using existing button
-            const existingButton = document.querySelector("button.profile-util-copy");
-            existingButton.click();
-
-            //manipulate HTML string and rewrite into clipboard
-            const html = await getHTMLFromClipboard();
-            const updatedHTML = await updateHTML(html, csvData);
-            await saveHTMLToClipboard(updatedHTML);
-        });
+        copyButtonAu.addEventListener("click",()=>handleCopy(COUNTRY_CODES.AU,csvData)); 
+        copyButtonFr.addEventListener("click",()=>handleCopy(COUNTRY_CODES.FR,csvData)); 
     }, 3000);
+}
+
+function setCopyButton(copyButton,countryCode){
+    const iconImg = document.createElement("img");
+    iconImg.src = chrome.runtime.getURL(`src/assets/icons/${countryCode}_copy.svg`);
+    iconImg.classList.add("copy-img");
+
+    copyButton.setAttribute('title','Copy details');
+    copyButton.appendChild(iconImg);
+}
+
+async function handleCopy(countryCode,csvData){
+    const existingButton = document.querySelector("button.profile-util-copy");
+    existingButton.click();
+
+    //manipulate HTML string and rewrite into clipboard
+    const html = await getHTMLFromClipboard();
+    let updatedHTML = await updateHTML(html, csvData);
+
+    if(countryCode==COUNTRY_CODES.FR) updatedHTML=translateToFrench(updatedHTML);
+
+    await saveHTMLToClipboard(updatedHTML);
 }
 
 function getFrRatesByAuStandard(auStandard, csvData) {
@@ -103,6 +124,19 @@ async function updateHTML(htmlString, csvData) {
     return updatedHTML;
 }
 
+function translateToFrench(html){
+    html=html
+    .replace("Hourly Rates:","Taux horaires :")
+    .replace("Standard:","Standard :")
+    .replace("Prepaid:","Prépayé :")
+    .replace("subject to","voir")
+    .replace("prepaid terms","prestation prépayée")
+    .replace("Location","Localisation")
+    .replaceAll("+ VAT","HT");
+
+    return html;
+}
+
 function updateRatesElements(bulletPointListElement, csvData) {
     const standardRateListItemElement = bulletPointListElement.querySelector("li:first-child");
     const prepaidRateListItemElement = bulletPointListElement.querySelector("li:last-child");
@@ -119,7 +153,7 @@ function updateRatesElements(bulletPointListElement, csvData) {
 
     standardRateListItemElement.innerHTML = standardRateListItemElement.innerHTML.replace(auStandardRate + "+GST", frRates.frStandard + " + VAT");
     prepaidRateListItemElement.innerHTML = prepaidRateListItemElement.innerHTML.replace(auPrepaidRate + "+GST", frRates.frPrepaid + " + VAT");
-    prepaidRateListItemElement.innerHTML = prepaidRateListItemElement.innerHTML.replace("40h", "37,5h");
+    prepaidRateListItemElement.innerHTML = prepaidRateListItemElement.innerHTML.replace("40h", "37.5h");
 
     const termsAndConditionsElement = prepaidRateListItemElement.querySelector("a");
     if (!termsAndConditionsElement) return;
